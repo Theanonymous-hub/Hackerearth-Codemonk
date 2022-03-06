cd256b7dfc5a999e0a04cb386a9853f7fa141dc0
Prasana.R <76907455+Theanonymous-hub@users.noreply.github.com>
2022-03-06 13:11:34 +0530

    Create Monk and Rotation.py

---
+++ Monk and Rotation.py
@@ -0,0 +1,13 @@
+t=int(input())
+while t!=0:
+    n,k = map(int,input().split())
+    arr=list(map(int,input().split()))
+    index=n-(k%n)
+    for i in range(index,n):
+        print(arr[i], end=" ")
+    for i in range(index):
+        print(arr[i], end=" ")       
+
+    print(" ")
+    t-=1
+    
